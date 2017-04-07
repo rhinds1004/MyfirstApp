92b46e1fb028a79d8c9103de53d67046bfc884bd
Robert Hinds <hindsr@uw.edu>
2017-04-07 13:38:32 -0700

    init commit for firstApp

---
+++ app/src/main/java/com/example/hinds/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,15 @@
+package com.example.hinds.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        //Get the intent that started this activity and extract the string
+    }
+}
